a7c0f88778a756c33915280183e26dcd417456d1
Alain Lefebvre <alain.lefebvre@mindgeek.com>
2014-08-11 11:41:56 -0400

    Testing new uuid module as generating from /dev/urandom is slow

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	linuxproc "github.com/c9s/goprocinfo/linux"
 	"github.com/facebookgo/grace/gracehttp"
+	uuid "code.google.com/p/go-uuid/uuid"
 	"io"
 	"log"
 	"net/http"
@@ -325,12 +326,15 @@ func loadConfig(filename string, conf *Config) error {
 }
 
 func getUDID() string {
+	/*
 	f, _ := os.Open("/dev/urandom")
 	b := make([]byte, 16)
 	f.Read(b)
 	f.Close()
 	uuid := fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
 	return uuid
+	*/
+	return uuid.NewUUID().String()
 }
 
 /****************************************************************/
